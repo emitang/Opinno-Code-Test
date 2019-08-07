34a8a0b6e03adfa1d0f6198c100fc9b39e20aa33
Emily Tang <emitang35@gmail.com>
2019-08-07 12:02:58 +0200

    generating template and verifying fixtures executed correctly

--- movie/views.py
+++ movie/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 
 # Create your views here.
+def movie_show(request):
+    movie = Movie.objects.order_by('rating')
+    return render(request, 'movie/movie_show.html', {'movie': movie})
