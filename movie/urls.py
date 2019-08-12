97d608e3b327e8386ff370e157428b69ef6367ec
Emily Tang <emitang35@gmail.com>
2019-08-12 08:29:32 +0200

    final aesthetics changes

--- movie/urls.py
+++ movie/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
 	path('', views.movie_show, name = 'movie_show'),
+	path('<int:pk>/', views.movie_detail, name = 'movie_detail'),
 ]
--- movie/views.py
+++ movie/views.py
@@ -4,5 +4,9 @@ from .models import Movie
 
 # Create your views here.
 def movie_show(request):
-    movie = Movie.objects.order_by('rating')
-    return render(request, 'movie/movie_show.html', {'movie': movie})
+    movies = Movie.objects.all()
+    return render(request, 'movie/movie_show.html', {'movies': movies})
+
+def movie_detail(request, pk):
+    movie = Movie.objects.get(pk=pk)
+    return render(request, 'movie/movie_detail.html', {'movie': movie})
